e2f80484963a7290eb1996a3bb92a7c124d1187b
BenitzCoding <benitzcoding@yahoo.com>
2022-02-23 22:58:47 +0800

    Deleted christmas code and added welcome message

--- cogs/events.py
+++ cogs/events.py
@@ -2,8 +2,10 @@ import cool_utils
 import discord
 import requests
 import os
+
 from imports import *
 from utilities import default
+from datetime import datetime
 from pymongo import MongoClient
 from discord.ext import commands
 from discord.ui import Button, button, View
@@ -15,38 +17,29 @@ class Events(commands.Cog):
 		self.senarc = senarc
 		self.config = default.get("./config.json")
 
-	@commands.Cog.listener('on_message')
-	async def chrismas_special(self, message):
-		if message.guild == None and message.content.lower() == "confirm":
-			data = {
-				"id": message.author.id,
-				"type": "Confirmed",
-				"auth": ""
-			}
-			res = requests.post("https://api.senarc.org/confirm-contest", json=data)
-			if res.status_code == 400:
-				return await message.author.send("Your coding contest project has already been processed.")
-			elif res.status_code == 401:
-				return
-			else:
-				return await message.author.send("Marked as `confirmed`!")
+	@commands.listener('on_message')
+	async def welcome_message(self, member):
+		if member.guild.id != 886543799843688498:
+			return
+		channel = discord.utils.get(member.guild.channels, id=886543799843688501)
+		embed = discord.Embed(timestamp=int(datetime.now().timestamp()), description="Have a nice stay at **Senarc**. You should read <#886752366962040843> before getting started!", colour=0x90B4F8)
+		embed.set_author(name=f"Welcome {member.name}!", icon_url=member.avatar_url)
 
-		elif message.guild == None and message.content.lower() == "cancel":
-			data = {
-				"id": message.author.id,
-				"type": "Cancelled",
-				"auth": ""
-			}
-			res = requests.post("https://api.senarc.org/confirm-contest", json=data)
-			if res.status_code == 400:
-				return await message.author.send("Your coding contest project has already been processed.")
-			elif res.status_code == 401:
-				return
-			else:
-				return await message.author.send("Marked as `cancelled`!")
+		member_count = len(member.guild.members)
+		if member_count.endswith("1"):
+			member_count = member_count + "st"
+
+		elif member_count.endswith("2"):
+			member_count = member_count + "nd"
+
+		elif member_count.endswith("3"):
+			member_count = member_count + "rd"
 
 		else:
-			return
+			member_count = member_count + "th"
+
+		embed.set_footer(text=f"You're the {member_count} member", icon_url=member.guild.icon_url)
+		await channel.send(embed=embed)
 
 def setup(bot):
 	bot.add_cog(Events(bot))
