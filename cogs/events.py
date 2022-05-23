e691c8adc65d32f4504b6dcdec7552a36af41770
BenitzCoding <benitzcoding@yahoo.com>
2022-05-23 16:56:11 +0800

    Enforcing pep-8 and organised imports.

--- cogs/events.py
+++ cogs/events.py
@@ -1,25 +1,32 @@
-import cool_utils
-import discord
 import os
+import cool_utils
 
-from utilities import default
+from discord import Embed
+from discord.ext.commands import Cog
+
 from datetime import datetime
-from pymongo import MongoClient
-from discord.ext import commands
 
+from utilities.default import get
 
-class Events(commands.Cog):
+class Events(Cog):
 	def __init__(self, senarc):
 		self.senarc = senarc
-		self.config = default.get("./config.json")
+		self.config = get("./config.json")
 
-	@commands.Cog.listener('on_message')
+	@Cog.listener('on_message')
 	async def welcome_message(self, member):
 		if member.guild.id != 886543799843688498:
 			return
 		channel = await self.senarc.fetch_channel(886543799843688501)
-		embed = discord.Embed(timestamp=int(datetime.now().timestamp()), description="Have a nice stay at **Senarc**. You should read <#886752366962040843> before getting started!", colour=0x90B4F8)
-		embed.set_author(name=f"Welcome {member.name}!", icon_url=member.display_avatar)
+		embed = Embed(
+			timestamp = int(datetime.now().timestamp()),
+			description = "Have a nice stay at **Senarc**. You should read <#886752366962040843> before getting started!",
+			colour = 0x90B4F8
+		)
+		embed.set_author(
+			name = f"Welcome {member.name}!",
+			icon_url = member.display_avatar
+		)
 
 		member_count = str(len(member.guild.members))
 		if member_count.endswith("1"):
@@ -34,8 +41,11 @@ class Events(commands.Cog):
 		else:
 			member_count = member_count + "th"
 
-		embed.set_footer(text=f"You're the {member_count} member", icon_url=member.guild.icon)
-		await channel.send(embed=embed)
+		embed.set_footer(
+			text = f"You're the {member_count} member",
+			icon_url = member.guild.icon
+		)
+		await channel.send(embed = embed)
 
 def setup(bot):
 	bot.add_cog(Events(bot))
